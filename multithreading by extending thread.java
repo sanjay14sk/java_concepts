c6e7a39df0db21297b88ce4f1808a48cc23d38b4
SANJAYKUMAR K <sanjay14col@gmail.com>
2026-01-20 21:32:41 +0530

    multithreading by extending thread.java

--- multithreading by extending thread.java
+++ multithreading by extending thread.java
@@ -1,1 +1,18 @@
+class ATM extends Thread {
+    public void run() {
+        System.out.println(
+            "Transaction handled by: " + Thread.currentThread().getName()
+        );
+    }
+
+    public static void main(String[] args) {
+        ATM user1 = new ATM();
+        ATM user2 = new ATM();
+        ATM user3 = new ATM();
+
+        user1.start();
+        user2.start();
+        user3.start();
+    }
+}
 
